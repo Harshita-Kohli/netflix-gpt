5d09933417c08096b717d4342b8e77291a240b8b
Harshita-Kohli <54809528+Harshita-Kohli@users.noreply.github.com>
2024-01-20 19:38:09 +0530

    Sign up and signin form and header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import './App.css';
+import Body from './components/Body';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold text-green-400">Hello World</h1>
-    </div>
+    <Body />
   );
 }
 
